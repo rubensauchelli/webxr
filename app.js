93a9f2d2e0479a6f1741daf704ad79432ae2099b
Stefano Rosà <35814537+R-Stefano@users.noreply.github.com>
2022-03-06 23:15:06 +0000

    test CI/CD

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+const compression = require('compression')
+const port = process.env.port || 8080
+const path=require('path')
+
+const app = express()
+
+app.use(compression())
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/'), {etag: false, lastModified: false}));
+
+//Serve frontend as static
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'dist/'), {etag: false, lastModified: false});
+});
+
+app.listen(port, () => console.log('Service server started. Listening on port '+ port));
---
+++ production.dockerfile
@@ -0,0 +1,10 @@
+FROM node:12-slim
+
+WORKDIR /usr/src/app
+
+ENV ENVIRONMENT=prod
+
+COPY . .
+RUN npm install
+
+CMD [ "npm", "run", "build:prod"]
